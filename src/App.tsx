6db0da32d88155b75b96f72ff6e3d1c8ac7df683
francescodesimone <f.desimone30@studenti.unisa.it>
2021-11-10 07:19:35 +0100

    Add frist requirement

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import SwipeableViews from 'react-swipeable-views';
+import { useTheme } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import TabPanel from './Components/TabPanel'
+import Carusel from './Components/Carusel'
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const a11yProps = (index: number) => {
+    return {
+        id: `full-width-tab-${index}`,
+        'aria-controls': `full-width-tabpanel-${index}`,
+    }
 }
 
-export default App;
+export default () => {
+    const theme = useTheme();
+    const [value, setValue] = React.useState(0);
+    const [data, setData] = React.useState({});
+
+    React.useEffect(() => {
+        axios.get("https://swapi.dev/api").then(resp => setData(resp.data))
+    }, [data])
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
+    const handleChangeIndex = (index: number) => {
+        setValue(index);
+    };
+    const appbar: string[] = Object.keys(data)
+    const content: string[] = Object.values(data)
+    return (
+        <Box sx={{ bgcolor: 'background.paper' }}>
+            <AppBar position="static">
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="secondary"
+                    textColor="inherit"
+                    variant="fullWidth"
+                    aria-label="full width tabs example"
+                >
+                    {appbar.map((data, index) => < Tab label={data} {...a11yProps(index)} />)}
+                </Tabs>
+            </AppBar>
+            <SwipeableViews
+                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                index={value}
+                onChangeIndex={handleChangeIndex}
+            >
+                {content.map((data, index) => <TabPanel value={value} index={index} dir={theme.direction} > <Carusel url={data}></Carusel></TabPanel>)}
+            </SwipeableViews>
+        </Box >
+    );
+}
+
---
+++ src/Components/Carusel.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import axios from 'axios';
+import TextField from '@mui/material/TextField';
+import { Box } from '@mui/material';
+import DescriptionModal from './DescriptionModal';
+
+// import * as ddg from "duckduckgo-images-api"
+
+interface ICarusel {
+    url: string;
+}
+
+export default (props: ICarusel) => {
+    // ddg.image_search({ query: "birds", moderate: true }).then(results => console.log(results))
+
+    const [data, setData] = React.useState<any>([{}]);
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const filtering = (text: any) => {
+        setData(data.results.filter((d: any) => d[Object.keys(d)[0]].includes(text.target.value)))
+    }
+    React.useEffect(() => {
+        axios.get(props.url).then(resp => setData([...data, resp.data]))
+        console.log(data.forEach((e: any) => console.log(e)))
+    }, [data])
+
+    if (data.some((el: any) => el.results != undefined))
+        return (
+            <Box>
+                <TextField fullWidth id="standard-basic" label="Search..." variant="standard" onChange={filtering} />
+                <ImageList cols={4} gap={40} sx={{ margin: 10 }}>
+                    {data.forEach((element: any) => {
+                        element.results.map((item: any, index: number) => (
+                            <ImageListItem key={index.toString()} onClick={handleOpen}>
+                                <img
+                                    src="https://wallpapercave.com/wp/nHaLQDm.gif?w=248&fit=crop&auto=format"
+                                    srcSet="https://wallpapercave.com/wp/nHaLQDm.gif?w=248&fit=crop&auto=format&dpr=2 2x"
+                                    alt={item[Object.keys(item)[0]]}
+                                    loading="lazy"
+                                />
+                                <ImageListItemBar
+                                    title={item[Object.keys(item)[0]]}
+                                />
+                                <DescriptionModal data={item} open={open} handleOpen={handleOpen} handleClose={handleClose} />
+                            </ImageListItem>
+                        ))
+                    })
+                    }
+                </ImageList >
+            </Box >
+        );
+    return null
+}
